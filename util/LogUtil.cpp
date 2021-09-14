36c05dc700c16821a275bf523fe6003d5ab7b33c
zhouchuren <zhouchuren198299@163.com>
2021-09-14 19:57:05 +0800

    2021/09/14

--- util/LogUtil.cpp
+++ util/LogUtil.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "LogUtil.h"
+#include <iostream>
+
+using namespace std;
+
+void LogUtil::log(string file, string function, long line, unsigned int level, string message) {
+	if (level == LOG_DEBUG) {
+		cout << "[D] ";
+	}
+	else if (level == LOG_ERROR) {
+		cout << "[E] ";
+	}
+
+	cout << message;
+	cout << " [" << file << ":" << function << ", " << line << "]" << endl;	
+}
+
+
