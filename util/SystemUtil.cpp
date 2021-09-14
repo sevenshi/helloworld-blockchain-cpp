68351fbd91213a8be61632ac0fa907adf10413b1
sevenshi <seven_shi@qq.com>
2021-09-14 14:01:20 +0800

    implement systemRootDirectory

--- util/SystemUtil.cpp
+++ util/SystemUtil.cpp
@@ -1,12 +1,15 @@
 //
 // Created by 40906 on 2021/9/12.
 //
-
+#include <iostream>
+#include <unistd.h>
 #include "SystemUtil.h"
 
 void SystemUtil::errorExit(string message, exception exception){
 
 }
 string SystemUtil::systemRootDirectory(){
-
+    string path;
+    path = getcwd(NULL,0);
+    return path;
 }
